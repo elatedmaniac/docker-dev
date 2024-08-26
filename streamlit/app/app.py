d7dddd240a0db937c5f82949324f910c10d0b1f2
elatedmaniac <maxaustin92@gmail.com>
2024-08-25 23:16:36 -0400

    Success modding CSS and font

--- streamlit/app/app.py
+++ streamlit/app/app.py
@@ -4,7 +4,10 @@ import time
 import ollama
 from llama_index.llms.ollama import Ollama
 from llama_index.core.llms import ChatMessage
-
+# Custom CSS for cyberpunk theme
+with open( "./css/main.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+    
 phi3_client = ollama.Client(host='http://phi3:11434')
 
 # Initialize session state for chat history
@@ -14,11 +17,16 @@ def initialize_session_state():
     if 'model' not in st.session_state:
         st.session_state['model'] = 'phi3:3.8b'
 # Function to generate response based on selected AI model
-def generate_response(prompt, model="phi3:3.8b"):
+def generate_response(prompt):
     response = ""
-    stream = phi3_client.chat(model=model, messages=[{'role': 'user', 'content': prompt}], stream=True,)
-    for chunk in stream:
+    response_placeholder = st.empty()
+    for chunk in phi3_client.chat(model=st.session_state['model'], 
+                              messages=[{'role': 'user', 'content': prompt}], 
+                              stream=True,options={"temperature": 0.5, "top_k": 10, "min_p": 0.1}):
+     
         response += chunk['message']['content']
+        response_placeholder.markdown(response + "▌")
+    response_placeholder.empty()
     return response
 
 def onclick_callback():
@@ -28,23 +36,20 @@ def onclick_callback():
     st.session_state.messages.append({"role": "user", "content": prompt})
     with messages.chat_message(f"{st.session_state['model']}  - assistant", avatar="🤖"):
         with st.spinner("Processing..."):
-            response = generate_response(prompt, st.session_state['model'])
+            response = generate_response(prompt)
             st.session_state.messages.append({"role": "assistant", "content": response})
-        
-
-# Custom CSS for cyberpunk theme
-with open( "./css/main.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 initialize_session_state()
 
-# Sidebar for API key input
+# Sidebar for model selection & parameter input
 st.sidebar.markdown("## Model Connection")
 models = [model["name"] for model in phi3_client.list()["models"]]
 # AI model selection
 st.session_state['model'] = st.sidebar.selectbox("Select Model", models)
+# TODO: Temperature, min P, and Top K sliders will go here
+temp = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0,step=0.1, value=0.5)
 
-st.title("Welcome to the AI Pod, Space Cowboy!")
+st.title("Welcome to the AI Pod, Space Cowboy! 🌵")
 st.markdown("## Models. Online.")
 messages = st.container()
 prompt_form = st.form("prompt-form",clear_on_submit=True)
@@ -57,6 +62,7 @@ for message in st.session_state.messages:
         avatar="🤠"
     else:
         avatar="🤖"
+        
     with messages.chat_message(message["role"], avatar=avatar):
         st.markdown(message["content"])
 
